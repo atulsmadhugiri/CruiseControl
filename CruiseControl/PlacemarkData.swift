00a24cb89f35ee0d062dadbf7c6ed96ffed6c027
Atul Madhugiri <atulm@berkeley.edu>
2023-12-21 15:46:44 -0800

    [ios] Introduce `PlacemarkData`

---
+++ CruiseControl/PlacemarkData.swift
@@ -0,0 +1,26 @@
+import CoreLocation
+
+struct PlacemarkData {
+  var areasOfInterest: [String]?
+  var locality: String?
+  var subLocality: String?
+  var administrativeArea: String?
+  var subAdministrativeArea: String?
+  var country: String?
+
+  init(
+    areasOfInterest: [String]? = nil,
+    locality: String? = nil,
+    subLocality: String? = nil,
+    administrativeArea: String? = nil,
+    subAdministrativeArea: String? = nil,
+    country: String? = nil
+  ) {
+    self.areasOfInterest = areasOfInterest
+    self.locality = locality
+    self.subLocality = subLocality
+    self.administrativeArea = administrativeArea
+    self.subAdministrativeArea = subAdministrativeArea
+    self.country = country
+  }
+}
