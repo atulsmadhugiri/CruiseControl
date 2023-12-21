f2c54d86fb18bcd5c7e77463c3772641d58a697c
Atul Madhugiri <atulm@berkeley.edu>
2023-12-20 22:47:32 -0800

    [ios] Tidy up with Prettier

--- CruiseControl/Item.swift
+++ CruiseControl/Item.swift
@@ -1,18 +1,11 @@
-//
-//  Item.swift
-//  CruiseControl
-//
-//  Created by atul on 12/20/23.
-//
-
 import Foundation
 import SwiftData
 
 @Model
 final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
+  var timestamp: Date
+
+  init(timestamp: Date) {
+    self.timestamp = timestamp
+  }
 }
