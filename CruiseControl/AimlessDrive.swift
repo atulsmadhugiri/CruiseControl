4b447fae662b21745255ea35f28284dcd9acfde7
Atul Madhugiri <atulm@berkeley.edu>
2023-12-20 23:03:22 -0800

    [ios] Extend `CLLocationCoordinate2D` to be `Codable`

--- CruiseControl/AimlessDrive.swift
+++ CruiseControl/AimlessDrive.swift
@@ -2,6 +2,28 @@ import CoreLocation
 import Foundation
 import SwiftData
 
+extension CLLocationCoordinate2D: Codable {
+
+  enum CodingKeys: String, CodingKey {
+    case latitude
+    case longitude
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+    let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+    self.init(latitude: latitude, longitude: longitude)
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(latitude, forKey: .latitude)
+    try container.encode(longitude, forKey: .longitude)
+  }
+
+}
+
 @Model
 final class AimlessDrive {
   var startTime: Date
