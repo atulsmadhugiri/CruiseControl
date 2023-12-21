9f193ab7a3fcfd649ca77bca4d8bc87a8bd3630f
Atul Madhugiri <atulm@berkeley.edu>
2023-12-21 01:31:31 -0800

    [ios] Remove `[start/end]Location` from `AimlessDrive`
    We can get these implicitly from `route`, there's no need to store separately.

--- CruiseControl/AimlessDrive.swift
+++ CruiseControl/AimlessDrive.swift
@@ -28,21 +28,15 @@ extension CLLocationCoordinate2D: Codable {
 final class AimlessDrive {
   var startTime: Date
   var endTime: Date
-  var startLocation: CLLocationCoordinate2D
-  var endLocation: CLLocationCoordinate2D
   var route: [CLLocationCoordinate2D]
 
   init(
     startTime: Date,
     endTime: Date,
-    startLocation: CLLocationCoordinate2D,
-    endLocation: CLLocationCoordinate2D,
     route: [CLLocationCoordinate2D]
   ) {
     self.startTime = startTime
     self.endTime = endTime
-    self.startLocation = startLocation
-    self.endLocation = endLocation
     self.route = route
   }
 }
--- CruiseControl/AimlessDriveDetailView.swift
+++ CruiseControl/AimlessDriveDetailView.swift
@@ -5,20 +5,24 @@ struct AimlessDriveDetailView: View {
   var aimlessDrive: AimlessDrive
   var body: some View {
     Map {
-      Annotation("Start", coordinate: aimlessDrive.startLocation, anchor: .bottom) {
-        Image(systemName: "flag.fill")
-          .padding(8)
-          .foregroundStyle(.white)
-          .background(Color.indigo)
-          .cornerRadius(4)
+      if let startingLocation = aimlessDrive.route.first {
+        Annotation("Start", coordinate: startingLocation, anchor: .bottom) {
+          Image(systemName: "flag.fill")
+            .padding(8)
+            .foregroundStyle(.white)
+            .background(Color.indigo)
+            .cornerRadius(4)
+        }
       }
 
-      Annotation("End", coordinate: aimlessDrive.endLocation, anchor: .bottom) {
-        Image(systemName: "flag.checkered")
-          .padding(8)
-          .foregroundStyle(.white)
-          .background(Color.orange)
-          .cornerRadius(4)
+      if let endingLocation = aimlessDrive.route.last {
+        Annotation("End", coordinate: endingLocation, anchor: .bottom) {
+          Image(systemName: "flag.checkered")
+            .padding(8)
+            .foregroundStyle(.white)
+            .background(Color.orange)
+            .cornerRadius(4)
+        }
       }
     }
   }
