03334ae908c63a23c0b8dc72274c41eb58d71d32
Atul Madhugiri <atulm@berkeley.edu>
2023-12-22 01:37:21 -0800

    [ios] Added `Map` preview to `AimlessDriveList`

--- CruiseControl/AimlessDriveList.swift
+++ CruiseControl/AimlessDriveList.swift
@@ -1,4 +1,5 @@
 import CoreLocation
+import MapKit
 import SwiftData
 import SwiftUI
 
@@ -13,9 +14,29 @@ struct AimlessDriveList: View {
           NavigationLink {
             AimlessDriveDetailView(aimlessDrive: drive)
           } label: {
-            Text(
-              "\(drive.name) completed on \(drive.endTime.formatted(date: .abbreviated, time: .shortened))"
-            )
+            HStack {
+              Map(interactionModes: []) {
+                MapPolyline(coordinates: drive.route)
+                  .stroke(
+                    .blue,
+                    style: StrokeStyle(
+                      lineWidth: 6,
+                      lineCap: .round,
+                      lineJoin: .round
+                    )
+                  )
+              }.frame(width: 120, height: 90)
+                .allowsHitTesting(false)
+                .cornerRadius(8.0)
+                .shadow(radius: 1.0)
+
+              VStack(alignment: .leading) {
+                Text(drive.name).font(.system(size: 20, design: .rounded).weight(.bold))
+                Text(drive.endTime.formatted(date: .abbreviated, time: .shortened))
+                  .font(.system(size: 12, design: .rounded))
+                Spacer()
+              }.padding()
+            }.padding(EdgeInsets(top: 8.0, leading: 0, bottom: 8.0, trailing: 0))
           }
         }
         .onDelete(perform: deleteItems)
