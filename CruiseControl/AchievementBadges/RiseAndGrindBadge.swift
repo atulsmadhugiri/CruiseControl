fa705003e052c267fc8886a4d23614e5e5c61990
Atul Madhugiri <atulm@berkeley.edu>
2023-12-23 04:09:55 -0800

    [ios] Introduce `RiseAndGrindBadge`

---
+++ CruiseControl/AchievementBadges/RiseAndGrindBadge.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct RiseAndGrindBadge: View {
+  var body: some View {
+    ZStack {
+      Rectangle()
+        .frame(width: 80, height: 80)
+        .cornerRadius(8.0)
+        .foregroundStyle(.orange)
+
+      Image(systemName: "sun.horizon.fill")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 50)
+        .foregroundStyle(.white)
+    }
+  }
+}
+
+#Preview {
+  RiseAndGrindBadge()
+}
