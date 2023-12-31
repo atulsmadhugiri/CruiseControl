575bbe00cda36031121d3fb19aa8fed429bb057f
Atul Madhugiri <atulm@berkeley.edu>
2023-12-31 02:35:18 -0800

    [ios] Introduce `ProfileViewModel`
    Largely modeled from https://developer.apple.com/documentation/photokit/bringing_photos_picker_to_your_swiftui_app.
    
    Unfortunately, `await avatarSelection?.loadTransferable(type: Image.self)` only works for PNGs. They didn't mention this in the WWDC video and in only one place info box in the docs... so we have to implement this `ProfileImage` stuff.

--- CruiseControl/ProfileView.swift
+++ CruiseControl/ProfileView.swift
@@ -1,5 +1,76 @@
+import CoreTransferable
 import PhotosUI
 import SwiftUI
+
+// Adapted from https://developer.apple.com/documentation/photokit/bringing_photos_picker_to_your_swiftui_app
+@MainActor
+class ProfileViewModel: ObservableObject {
+  @Published var firstName: String = ""
+  @Published var lastName: String = ""
+  @Published var biography: String = ""
+
+  enum ImageState {
+    case empty
+    case loading(Progress)
+    case success(Image)
+    case failure(Error)
+  }
+
+  @Published private(set) var imageState: ImageState = .empty
+
+  enum TransferError: Error {
+    case importFailed
+  }
+
+  struct ProfileImage: Transferable {
+    let image: Image
+
+    static var transferRepresentation: some TransferRepresentation {
+      DataRepresentation(importedContentType: .image) { data in
+
+        guard let uiImage = UIImage(data: data) else {
+          throw TransferError.importFailed
+        }
+
+        let image = Image(uiImage: uiImage)
+        return ProfileImage(image: image)
+
+      }
+    }
+  }
+
+  @Published var imageSelection: PhotosPickerItem? = nil {
+    didSet {
+      if let imageSelection {
+        let progress = loadTransferable(from: imageSelection)
+        imageState = .loading(progress)
+      } else {
+        imageState = .empty
+      }
+    }
+  }
+
+  private func loadTransferable(from imageSelection: PhotosPickerItem) -> Progress {
+    return imageSelection.loadTransferable(type: ProfileImage.self) { result in
+
+      DispatchQueue.main.async {
+        guard imageSelection == self.imageSelection else {
+          print("Failed to get the selected item.")
+          return
+        }
+        switch result {
+        case .success(let profileImage?):
+          self.imageState = .success(profileImage.image)
+        case .success(.none):
+          self.imageState = .empty
+        case .failure(let error):
+          self.imageState = .failure(error)
+        }
+      }
+
+    }
+  }
+}
 
 struct ProfileView: View {
   @State private var avatarSelection: PhotosPickerItem?
