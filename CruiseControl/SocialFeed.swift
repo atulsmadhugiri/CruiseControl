ddac21a425647fcaf3877ce4269f82aae86ab209
Atul Madhugiri <atulm@berkeley.edu>
2023-12-30 21:39:18 -0800

    [ios] Add placeholder social buttons to `SocialFeed` cards

--- CruiseControl/SocialFeed.swift
+++ CruiseControl/SocialFeed.swift
@@ -59,6 +59,24 @@ struct SocialFeed: View {
             .allowsHitTesting(false)
             .cornerRadius(8.0)
             .shadow(radius: 1.0)
+
+          HStack {
+            Button {
+              print("UNIMPLEMENTED")
+            } label: {
+              Image(systemName: "heart.fill").frame(height: 20).foregroundColor(.pink)
+              Text("3").foregroundColor(.pink)
+            }.buttonStyle(.bordered).tint(.secondary)
+            Button {
+              print("UNIMPLEMENTED")
+            } label: {
+              HStack {
+                Image(systemName: "bubble.fill").frame(height: 20)
+                Text("Comment")
+              }.frame(maxWidth: .infinity)
+            }.buttonStyle(.bordered).tint(.secondary)
+          }
+
         }
 
       }
