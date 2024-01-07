a43dc25c7c867f28dbe48060ba437b52cbe1a7aa
Atul Madhugiri <atulm@berkeley.edu>
2024-01-06 19:35:29 -0500

    [ios] Introduce `fetchAuthorProfile()` to `PostAuthorView`

--- CruiseControl/Social/SocialFeedPostAuthorView.swift
+++ CruiseControl/Social/SocialFeedPostAuthorView.swift
@@ -3,13 +3,34 @@ import SwiftUI
 
 struct SocialFeedPostAuthorView: View {
   var creator: CKRecord.ID?
+  @State var firstName: String?
+  @State var lastName: String?
+
   var body: some View {
     HStack {
-      Text("\(creator?.recordName ?? "Unknown")".suffix(10))
+      Text("\(firstName ?? "Anonymous") \(lastName ?? "")")
       Image(systemName: "checkmark.seal.fill")
         .foregroundColor(.yellow)
         .frame(width: 12)
         .font(.system(size: 14))
+    }.onAppear {
+      Task {
+        await fetchAuthorProfile()
+      }
     }
   }
+
+  func fetchAuthorProfile() async {
+    guard let creator else { return }
+    let existingRecord = await potentiallyGetUserProfileRecord(userRecordID: creator)
+    if let existingRecord {
+      if let firstName = existingRecord.value(forKey: "firstName") as? String {
+        self.firstName = firstName
+      }
+      if let lastName = existingRecord.value(forKey: "lastName") as? String {
+        self.lastName = lastName
+      }
+    }
+  }
+
 }
