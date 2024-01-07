46694dcad411511ecbda79a1b10ac16b3a3a25e1
Atul Madhugiri <atulm@berkeley.edu>
2024-01-06 21:17:42 -0500

    [ios] Add haptics and animations to "like" button
    Add some haptics and animations on "like." It was cool to get them working so easily, but they might be a little excessive... will see what feedback we get.

--- CruiseControl/Social/SocialFeedPostEngagement.swift
+++ CruiseControl/Social/SocialFeedPostEngagement.swift
@@ -1,14 +1,27 @@
 import SwiftUI
 
 struct SocialFeedPostEngagement: View {
+  @State var liked: Bool = false
+  @State var likeCount: Int = 2
+  @State var bounceValue: Bool = false
+  let hapticFeedback = UIImpactFeedbackGenerator(style: .heavy)
+
   var body: some View {
     HStack {
       Button {
-        print("UNIMPLEMENTED")
+        likeCount = liked ? likeCount - 1 : likeCount + 1
+        bounceValue = !liked ? !bounceValue : bounceValue
+        liked = !liked
+        hapticFeedback.impactOccurred()
       } label: {
-        Image(systemName: "heart.fill").frame(height: 20).foregroundColor(.pink)
-        Text("3").foregroundColor(.pink)
-      }.buttonStyle(.bordered).tint(.secondary)
+        Image(systemName: "heart.fill")
+          .frame(height: 20)
+          .foregroundColor(liked ? .pink : .none)
+          .symbolEffect(.bounce, value: bounceValue)
+        Text("\(likeCount)")
+          .foregroundColor(liked ? .pink : .none)
+          .contentTransition(.numericText(countsDown: liked))
+      }.buttonStyle(.bordered).tint(liked ? .pink : .secondary)
       Button {
         print("UNIMPLEMENTED")
       } label: {
@@ -17,6 +30,8 @@ struct SocialFeedPostEngagement: View {
           Text("Comment")
         }.frame(maxWidth: .infinity)
       }.buttonStyle(.bordered).tint(.secondary)
+    }.onAppear {
+      hapticFeedback.prepare()
     }
   }
 }
