f17634c8bcb983a5a6f5b01d3416c05e6bb6152b
Atul Madhugiri <atulm@berkeley.edu>
2024-02-01 18:18:34 -0500

    [ios] Introduce simple comment composer in `CommentSheet`
    This will probably be its own component at some point. Need to add some animation and color.

--- CruiseControl/Social/SocialFeedCommentSheet.swift
+++ CruiseControl/Social/SocialFeedCommentSheet.swift
@@ -1,8 +1,24 @@
 import SwiftUI
 
 struct SocialFeedCommentSheet: View {
+  @State private var comment = ""
+
   var body: some View {
-    Text("`SocialFeedCommentSheet`")
+    VStack {
+      Spacer()
+      HStack {
+      }
+    }.safeAreaInset(edge: .bottom) {
+      HStack {
+        TextField("Write a comment...", text: $comment).frame(
+          width: 300
+        ).textFieldStyle(.roundedBorder)
+        Button {
+        } label: {
+          Image(systemName: "paperplane.fill").frame(height: 20)
+        }.buttonStyle(.bordered).tint(.secondary)
+      }.padding()
+    }
   }
 }
 
