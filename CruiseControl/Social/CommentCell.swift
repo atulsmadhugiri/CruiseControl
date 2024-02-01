f375b95b6ead8249cefdc81868a0f63d45380db0
Atul Madhugiri <atulm@berkeley.edu>
2024-02-01 18:56:28 -0500

    [ios] Introduce placeholder `CommentCell`

--- CruiseControl/Social/CommentCell.swift
+++ CruiseControl/Social/CommentCell.swift
@@ -2,7 +2,25 @@ import SwiftUI
 
 struct CommentCell: View {
   var body: some View {
-    Text("`CommentCell`")
+    HStack(alignment: .top) {
+      AsyncImage(url: URL(string: "https://blob.sh/atul.png")) { image in
+        image.resizable()
+          .frame(width: 40, height: 40)
+          .cornerRadius(8.0)
+      } placeholder: {
+        Color.gray.opacity(0.3).frame(width: 40, height: 40).cornerRadius(8.0)
+      }
+
+      VStack(alignment: .leading) {
+        HStack {
+          Text("Atul Madhugiri").lineLimit(1).font(.caption2).bold()
+          Text(Date().formatted()).font(.caption2).foregroundStyle(.secondary)
+        }
+        Text(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        ).font(.caption)
+      }
+    }.padding()
   }
 }
 
