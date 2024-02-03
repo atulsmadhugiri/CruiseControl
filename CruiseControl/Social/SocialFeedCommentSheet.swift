310521849cceb2416bb78832288ee0d60e00490b
Atul Madhugiri <atulm@berkeley.edu>
2024-02-02 20:07:46 -0500

    [ios] Introduce `fetchPostComments` to `CommentSheet`
    Fetch comments in chronological order for given `DrivePost`.

--- CruiseControl/Social/SocialFeedCommentSheet.swift
+++ CruiseControl/Social/SocialFeedCommentSheet.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 struct SocialFeedCommentSheet: View {
   var drivePostID: CKRecord.ID?
+  @State var postComments: [PostComment] = []
   @State private var comment = ""
 
   let commentFeedback = UIImpactFeedbackGenerator(style: .heavy)
@@ -39,8 +40,47 @@ struct SocialFeedCommentSheet: View {
         .background(.thinMaterial)
     }.onAppear {
       commentFeedback.prepare()
+      Task {
+        await fetchPostComments()
+      }
     }
   }
+
+  func fetchPostComments() async {
+    guard let drivePostID else {
+      return
+    }
+
+    do {
+      let publicDB = CKContainer.default().publicCloudDatabase
+      let reference = CKRecord.Reference(recordID: drivePostID, action: .none)
+      let predicate = NSPredicate(format: "drivePostRef == %@", reference)
+      let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: true)
+
+      let query = CKQuery(recordType: "PostCommentAlpha", predicate: predicate)
+      query.sortDescriptors = [sortDescriptor]
+
+      let (matchResults, _) = try await publicDB.records(matching: query)
+      var fetchedComments: [PostComment] = []
+
+      for (_, recordResult) in matchResults {
+        switch recordResult {
+        case .success(let record):
+          if let postComment = PostComment(from: record) {
+            fetchedComments.append(postComment)
+          }
+        case .failure(let error):
+          print("Error fetching PostReaction record: \(error.localizedDescription)")
+        }
+      }
+
+      self.postComments = fetchedComments
+
+    } catch {
+      print("Error potentially fetching PostComments: \(error.localizedDescription)")
+    }
+  }
+
 }
 
 #Preview {
