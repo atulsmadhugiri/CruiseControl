34aee3a5d66a080ea0ccbbb4fb005c4fcd749010
Atul Madhugiri <atulm@berkeley.edu>
2024-02-19 15:25:25 -0500

    [ios] Temporarily remove `Map` from `AimlessDriveListCell`
    We want to see if we can find a repro for the issue @sridmad is facing. Before we can do that, we're trying to narrow down what might be causing the application to be killed by watchdog. The `Map` components in `AimlessDriveListCell` seem to use a decent amount of memory, so want to see if removing them makes any difference.

--- CruiseControl/AimlessDriveListCell.swift
+++ CruiseControl/AimlessDriveListCell.swift
@@ -5,21 +5,6 @@ struct AimlessDriveListCell: View {
   var drive: AimlessDrive
   var body: some View {
     HStack {
-      Map(interactionModes: []) {
-        MapPolyline(coordinates: drive.route)
-          .stroke(
-            .blue,
-            style: StrokeStyle(
-              lineWidth: 6,
-              lineCap: .round,
-              lineJoin: .round
-            )
-          )
-      }.frame(width: 120, height: 90)
-        .allowsHitTesting(false)
-        .cornerRadius(8.0)
-        .shadow(radius: 1.0)
-
       VStack(alignment: .leading) {
         Text(drive.name).font(.system(size: 20, design: .rounded).weight(.bold))
         Text(formattedDistanceTraveled(distance: drive.distanceTraveled))
