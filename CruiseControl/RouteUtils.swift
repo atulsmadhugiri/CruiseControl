e81688bda6c547975fbfa09318dbc384291e6785
Atul Madhugiri <atulm@berkeley.edu>
2024-02-26 01:24:27 -0500

    [ios] Simplify `distanceTraveled(...)` using `.adjacentPairs()`
    Just to start applying some of the utilities from `swift-algorithms`

--- CruiseControl/RouteUtils.swift
+++ CruiseControl/RouteUtils.swift
@@ -1,19 +1,18 @@
+import Algorithms
 import Foundation
 import MapKit
 
 func distanceTraveled(route: [CLLocationCoordinate2D]) -> CLLocationDistance {
   guard !route.isEmpty else { return 0 }
 
-  var distance: CLLocationDistance = 0
-  var pointA = CLLocation(coordinate: route[0])
-
-  for idx in 1..<route.count {
-    let pointB = CLLocation(coordinate: route[idx])
-    distance += pointA.distance(from: pointB)
-    pointA = pointB
+  var totalDistance: CLLocationDistance = 0
+  for (pointA, pointB) in route.adjacentPairs() {
+    let startLocation = CLLocation(coordinate: pointA)
+    let endLocation = CLLocation(coordinate: pointB)
+    totalDistance += startLocation.distance(from: endLocation)
   }
 
-  return distance
+  return totalDistance
 }
 
 func formattedDistanceTraveled(distance: CLLocationDistance) -> String {
